29655c9211cb7031ac06c86ddbeb84df5aada423
568xiaoma <32442905+568xiaoma@users.noreply.github.com>
2020-09-09 21:52:44 +0800

    Update map.vue

--- src/components/road/map.vue
+++ src/components/road/map.vue
@@ -50,21 +50,48 @@
         路段信息
         <el-button v-if="roadData" type="text" @click="updateRoadInfo">保存修改</el-button>
       </div>
-      <div v-if="roadData">
-        <el-input placeholder="请输入内容" v-model="roadData.part.region" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.part.road" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.part.roadNum" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.attribute.length" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.attribute.level" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.attribute.note" :disabled="true"></el-input>
-        <el-input placeholder="请输入内容" v-model="roadData.attribute.type" :disabled="true"></el-input>
+      <div v-if="roadData" class="side-bar-head">
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">区域:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.part.region" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">道路:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.part.road" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">车道数:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.part.roadNum" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">路段长度:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.attribute.length" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">损坏等级:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.attribute.level" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">备注:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.attribute.note" :disabled="false" size="mini" style="margin-right: 10px"></el-input>
+        </div>
+        <div class="side-bar-head-raw">
+          <div class="side-bar-head-title">路面类型:</div>
+          <el-input placeholder="请输入内容" v-model="roadData.attribute.type?'沥青路':'水泥路'" :disabled="true" size="mini" style="margin-right: 10px"></el-input>
+        </div>
       </div>
       <el-tabs v-if="roadData" class="side-bar-content" v-model="activeName" @tab-click="handleClick" tab-position="right" type="border-card" :stretch="true">
         <el-tab-pane v-for="index in roadData.part.roadNum" :label="'车道 '+String(index)" :name="String(index)" :key="index">
           <el-collapse v-model="activeNames" accordion>
+<<<<<<< HEAD
+            <el-collapse-item v-for="(option, i) in PitchTornOptions" :title="option.title" :name="String(i)">
+              <el-form :model="roadData.streets[index-1]" label-width="50px" size="mini">
+                <el-form-item v-for="content in option.content" :label="content.name" class="title-font-size">
+=======
             <el-collapse-item v-for="(option, i) in PitchTornOptions" :title="option.title" :name="String(i)" :key="i">
               <el-form :model="roadData.streets[index-1]" label-width="50px"  size="mini" :key="index">
                 <el-form-item v-for="content in option.content" :label="content.name" class="title-font-size" :key="content.name">
+>>>>>>> 946bbfcdc1c28a15437d01ce7655f226142cf27f
                   <el-select v-model="roadData.streets[index-1].torn[option.idx][content.idx]" placeholder="请选择损坏等级">
                     <el-option v-for="l in 4" :label="'level '+String(l-1)" :value="l-1" :key="l"></el-option>
                   </el-select>
@@ -79,6 +106,25 @@
 </template>
 
 <style>
+.side-bar-content .el-tabs__content{
+  padding: 0px 15px 0px 15px;
+}
+.side-bar-head-title{
+  width: 80px;
+  padding: 0px 10px 0px 10px;
+}
+.side-bar-head-raw{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  padding-bottom: 10px;
+}
+.side-bar-head{
+  background-color: #eeeeee;
+  padding: 10px 10px 5px 10px;
+}
 .side-bar-submit-button{
   position: absolute;
   bottom: 50px;
@@ -87,11 +133,15 @@
 .title-font-size .el-form-item__label{
   font-size: 12px;
 }
+.title-font-size-head .el-form-item__label{
+  font-size: 12px;
+}
 .side-bar-choice-title{
   height: 20px;
 }
 .side-bar-content{
-  height: 91vh;
+  height: 100%;
+  overflow: scroll;
 }
 .side-bar{
   position: absolute;
@@ -101,6 +151,8 @@
   height: 100vh;
   z-index: 2;
   background-color: white;
+  display: flex;  
+  flex-direction: column;
 }
 .side-bar-title{
   height: 8vh;
@@ -112,33 +164,33 @@
   font-weight: bold;
   /* border-bottom: 1px rgb(187, 187, 187) solid; */
 }
-  .amap-demo {
-    height: 300px;
-  }
-  .el-vue-amap-container.amap-demo{
-    width: 80%;
-    height: 100vh;
-  }
-  .search-box {
-    position: absolute;
-    top: 25px;
-    left: 20px;
-  }
-  .amap-page-container {
-    position: relative;
-  }
-  .info{
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-  }
-  .toolbar{
-    position: absolute;
-    z-index: 2;
-    top: 25px;
-    left: 20px;
-  }
+.amap-demo {
+  height: 300px;
+}
+.el-vue-amap-container.amap-demo{
+  width: 80%;
+  height: 100vh;
+}
+.search-box {
+  position: absolute;
+  top: 25px;
+  left: 20px;
+}
+.amap-page-container {
+  position: relative;
+}
+.info{
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+.toolbar{
+  position: absolute;
+  z-index: 2;
+  top: 25px;
+  left: 20px;
+}
 </style>
 
 <script>
@@ -185,259 +237,256 @@ const CementJointOptions = ['接缝料损坏', '边角剥落'];
 const CementSurfaceOptions = ['坑洞', '表面纹裂', '层状剥落'];
 const CementOtherOptions = ['错台', '拱胀', '唧浆', '路框差', '沉陷'];
 
-import qs from "qs"
 export default {
-    data(){
-      return {
-        PitchCrackOptions: [{name: '线裂', idx: 'c1'},
-                                  {name: '网裂', idx: 'c2'},
-                                  {name: '龟裂', idx: 'c3'},
-                                  ],
-        PitchShapeOptions: [{name: '拥包', idx: 's1'},
-                                  {name: '车辙', idx: 's2'},
-                                  {name: '沉陷', idx: 's3'},
-                                  {name: '翻浆', idx: 's4'},
-                                  ],
-        PitchLooseOptions: [{name: '剥落', idx: 'l1'},
-                                  {name: '坑槽', idx: 'l2'},
-                                  {name: '啃边', idx: 'l3'},
-                                  ],
-        PitchOtherOptions: [{name: '路框差', idx: 'o1'},
-                                  {name: '唧浆', idx: 'o2'},
-                                  {name: '泛油', idx: 'o3'},
-                                  ],
-        PitchTornOptions: [{
-            title: '有裂缝',
-            idx: 'crack',
-            content: PitchCrackOptions,
-          },{
-            title: '有变形',
-            idx: 'shape',
-            content: PitchShapeOptions,
-          },{
-            title: '有松散',
-            idx: 'loose',
-            content: PitchLooseOptions,
-          },{
-            title: '其他情况',
-            idx: 'others',
-            content: PitchOtherOptions,
-          },
-        ],
-        roadData: false,
-        streets:[
-          {
-            rid: 'abc002',
-            index: 1,
-            torn: {
-              crack:{
-                c1: 1,
-                c2: 1,
-                c3: 1,
-              },
-              shape:{
-                s1: 1,
-                s2: 1,
-                s3: 1,
-                s4: 1,
-              },
-              loose:{
-                l1: 1,
-                l2: 1,
-                l3: 1,
-              },
-              other:{
-                o1: 1,
-                o2: 1,
-                o3: 1,
-              }
-            }
-          },
-          {
-            rid: 'abc002',
-            index: 1,
-            torn: {
-              crack:{
-                c1: 1,
-                c2: 1,
-                c3: 1,
-              },
-              shape:{
-                s1: 1,
-                s2: 1,
-                s3: 1,
-                s4: 1,
-              },
-              loose:{
-                l1: 1,
-                l2: 1,
-                l3: 1,
-              },
-              other:{
-                o1: 1,
-                o2: 1,
-                o3: 1,
-              }
-            }
-          },
-          {
-            rid: 'abc002',
-            index: 1,
-            torn: {
-              crack:{
-                c1: 1,
-                c2: 1,
-                c3: 1,
-              },
-              shape:{
-                s1: 1,
-                s2: 1,
-                s3: 1,
-                s4: 1,
-              },
-              loose:{
-                l1: 1,
-                l2: 1,
-                l3: 1,
-              },
-              other:{
-                o1: 1,
-                o2: 1,
-                o3: 1,
-              }
-            }
-          },
-          {
-            rid: 'abc002',
-            index: 1,
-            torn: {
-              crack:{
-                c1: 1,
-                c2: 1,
-                c3: 1,
-              },
-              shape:{
-                s1: 1,
-                s2: 1,
-                s3: 1,
-                s4: 1,
-              },
-              loose:{
-                l1: 1,
-                l2: 1,
-                l3: 1,
-              },
-              other:{
-                o1: 1,
-                o2: 1,
-                o3: 1,
-              }
-            }
-          },
-        ],
-        activeNames: [],
-        activeName: '',
-        status: [1, 0, 1],
-        plugin:[{
-          pName: 'MapType',
-          defaultType: 1,
-          showRoad: true,
-          events: {
-            init(instance) {
-              console.log(instance);
-            }
-          }
-        }],
-        form:{
-          name: '',
-          targetid: '',
+  data(){
+    return {
+      PitchCrackOptions: [{name: '线裂', idx: 'c1'},
+                                {name: '网裂', idx: 'c2'},
+                                {name: '龟裂', idx: 'c3'},
+                                ],
+      PitchShapeOptions: [{name: '拥包', idx: 's1'},
+                                {name: '车辙', idx: 's2'},
+                                {name: '沉陷', idx: 's3'},
+                                {name: '翻浆', idx: 's4'},
+                                ],
+      PitchLooseOptions: [{name: '剥落', idx: 'l1'},
+                                {name: '坑槽', idx: 'l2'},
+                                {name: '啃边', idx: 'l3'},
+                                ],
+      PitchOtherOptions: [{name: '路框差', idx: 'o1'},
+                                {name: '唧浆', idx: 'o2'},
+                                {name: '泛油', idx: 'o3'},
+                                ],
+      PitchTornOptions: [{
+          title: '有裂缝',
+          idx: 'crack',
+          content: PitchCrackOptions,
+        },{
+          title: '有变形',
+          idx: 'shape',
+          content: PitchShapeOptions,
+        },{
+          title: '有松散',
+          idx: 'loose',
+          content: PitchLooseOptions,
+        },{
+          title: '其他情况',
+          idx: 'others',
+          content: PitchOtherOptions,
         },
-        PitchDialogVisible: false,
-        CementDialogVisible: false,
-        resizeEnable: true,
-        rotateEnable:true,
-        pitchEnable:true,
-        pitch:80,
-        rotation:-15,
-        viewMode: '3D',
-        expandZoomRange: true,
-        zooms: [3, 20],
-        zoom: 16,
-        center: [114.22109, 30.729849],
-        lng: 0.0,
-        lat: 0.0,
-        NowStreet: 1,
-        PitchRoadInfo: [],
-        CementRoadInfo: [],
-        // 在地图上画多边形
-        polygons: [
-          {
-            draggable: true,
-            // path: [[114.21106824846075, 30.731150108132034], [114.21125932978109, 30.73072762352457], [114.2111748328596, 30.730689407260503], [114.21098375153926, 30.731111891867968]],
-            path: [],
-            rid: '156485',
-            events: {
-              click:(e) => {
-                self.form.targetid = e.target.getExtData()
-            }
-          },
-          }
-        ],
-
-        // 在地图上画直线(该功能已删除，改成画多边形)
-        polyline: {
-          mypath: [[]],
-          events: {
-            click(e) {
-              alert('click polyline');
-              // console.log(self.lng, self.lat)
-              self.sendlnglat();
+      ],
+      roadData: false,
+      streets:[
+        {
+          rid: 'abc002',
+          index: 1,
+          torn: {
+            crack:{
+              c1: 1,
+              c2: 1,
+              c3: 1,
             },
-            end: (e) => {
-              let newPath = e.target.getPath().map(point => [point.lng, point.lat]);
-              console.log(newPath);
+            shape:{
+              s1: 1,
+              s2: 1,
+              s3: 1,
+              s4: 1,
+            },
+            loose:{
+              l1: 1,
+              l2: 1,
+              l3: 1,
+            },
+            other:{
+              o1: 1,
+              o2: 1,
+              o3: 1,
             }
-          },
-          editable: false
+          }
         },
+        {
+          rid: 'abc002',
+          index: 1,
+          torn: {
+            crack:{
+              c1: 1,
+              c2: 1,
+              c3: 1,
+            },
+            shape:{
+              s1: 1,
+              s2: 1,
+              s3: 1,
+              s4: 1,
+            },
+            loose:{
+              l1: 1,
+              l2: 1,
+              l3: 1,
+            },
+            other:{
+              o1: 1,
+              o2: 1,
+              o3: 1,
+            }
+          }
+        },
+        {
+          rid: 'abc002',
+          index: 1,
+          torn: {
+            crack:{
+              c1: 1,
+              c2: 1,
+              c3: 1,
+            },
+            shape:{
+              s1: 1,
+              s2: 1,
+              s3: 1,
+              s4: 1,
+            },
+            loose:{
+              l1: 1,
+              l2: 1,
+              l3: 1,
+            },
+            other:{
+              o1: 1,
+              o2: 1,
+              o3: 1,
+            }
+          }
+        },
+        {
+          rid: 'abc002',
+          index: 1,
+          torn: {
+            crack:{
+              c1: 1,
+              c2: 1,
+              c3: 1,
+            },
+            shape:{
+              s1: 1,
+              s2: 1,
+              s3: 1,
+              s4: 1,
+            },
+            loose:{
+              l1: 1,
+              l2: 1,
+              l3: 1,
+            },
+            other:{
+              o1: 1,
+              o2: 1,
+              o3: 1,
+            }
+          }
+        },
+      ],
+      activeNames: '0',
+      activeName: '1',
+      status: [1, 0, 1],
+      plugin:[{
+        pName: 'MapType',
+        defaultType: 1,
+        showRoad: true,
+        events: {
+          init(instance) {
+            console.log(instance);
+          }
+        }
+      }],
+      form:{
+        name: '',
+        targetid: '',
+      },
+      PitchDialogVisible: false,
+      CementDialogVisible: false,
+      resizeEnable: true,
+      rotateEnable:true,
+      pitchEnable:true,
+      pitch:80,
+      rotation:-15,
+      viewMode: '3D',
+      expandZoomRange: true,
+      zooms: [3, 20],
+      zoom: 16,
+      center: [114.22109, 30.729849],
+      lng: 0.0,
+      lat: 0.0,
+      // 在地图上画多边形
+      polygons: [
+        {
+          draggable: true,
+          // path: [[114.21106824846075, 30.731150108132034], [114.21125932978109, 30.73072762352457], [114.2111748328596, 30.730689407260503], [114.21098375153926, 30.731111891867968]],
+          path: [],
+          rid: '156485',
+          events: {
+            click:(e) => {
+              self.form.targetid = e.target.getExtData()
+          }
+        },
+        }
+      ],
 
-        // 点击地图发生事件
+      // 在地图上画直线(该功能已删除，改成画多边形)
+      polyline: {
+        mypath: [[]],
         events: {
           click(e) {
-            let { lng, lat } = e.lnglat;
-            mypaths = self.polygons;
-            mylength = mypaths.length
-            console.log('length: ', mylength)
-            self.polygons[mylength-1].path.push([lng, lat]);
-            // console.log('polygons: ', mypaths[mylength-1])
-
-            var url = 'static/hcx-coords.json';
-            self.$axios.get(url).then(res =>{
-              // console.log(res.data['sjg-test-0'])
-              var testdata = res.data['sjg-test-0'];
-              self.mypath=res.data['sjg-test-0'];
-              // console.log(self.mypath)
-            })
+            alert('click polyline');
+            // console.log(self.lng, self.lat)
+            self.sendlnglat();
           },
-          // 右键完成当前的区域规划
-          rightclick(e){
-            mypaths = self.polygons;
-            mylength = mypaths.length;
-            if(self.polygons[mylength-1].path.length){
-              self.polygons[mylength-1].key = "key"; // 这里的key要根据之后统一的道路命名规则来进行修改
-              self.newpath();
-            }
-            console.log(self.polygons)
+          end: (e) => {
+            let newPath = e.target.getPath().map(point => [point.lng, point.lat]);
+            console.log(newPath);
           }
         },
-      };
-    },
+        editable: false
+      },
+
+      // 点击地图发生事件
+      events: {
+        click(e) {
+          let { lng, lat } = e.lnglat;
+          mypaths = self.polygons;
+          mylength = mypaths.length
+          console.log('length: ', mylength)
+          self.polygons[mylength-1].path.push([lng, lat]);
+          // console.log('polygons: ', mypaths[mylength-1])
+
+          var url = 'static/hcx-coords.json';
+          self.$axios.get(url).then(res =>{
+            // console.log(res.data['sjg-test-0'])
+            var testdata = res.data['sjg-test-0'];
+            self.mypath=res.data['sjg-test-0'];
+            // console.log(self.mypath)
+          })
+        },
+        // 右键完成当前的区域规划
+        rightclick(e){
+          mypaths = self.polygons;
+          mylength = mypaths.length;
+          if(self.polygons[mylength-1].path.length){
+            self.polygons[mylength-1].key = "key"; // 这里的key要根据之后统一的道路命名规则来进行修改
+            self.newpath();
+          }
+          console.log(self.polygons)
+        }
+      },
+    };
+  },
     methods: {
       // 初始化沥青路面信息
       handleClick(tab, event) {
         console.log(tab, event);
       },
       updateRoadInfo(){
+        console.log(this.roadData)
         this.$axios.post(config.IP + '/road/infoUpdate', this.roadData) //向后端更新道路信息
         .then((response) => {
           console.log(response)
