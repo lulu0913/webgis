0ad84ac488684f73c1cd867bd6d64fc7a39bd20f
Zam-0703 <Zam-0703@outlook.com>
2020-10-08 14:45:54 +0800

    import csv and output csv

--- src/components/User/components/AdminRoadForm.vue
+++ src/components/User/components/AdminRoadForm.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="RoadForm">
+    <div>
+
+      <el-button style="margin-left: 10px;" type="success" @click="openCsvDialog()" icon="el-icon-plus">
+      导入路段信息
+      </el-button>
+      <el-dialog
+      :title="csvTitle"
+      :visible.sync="csvVisible"
+      :append-to-body="true"
+      width="50%">
+      <div>
+          <el-form ref="file" label-width="120px">
+            <center><input type="file" id="fileupload" name="csvfile" ref="csvData" /></center>
+          </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="csvVisible = false">取消</el-button>
+      <el-button type="primary" @click="csv">导入</el-button>
+      </span>
+      </el-dialog>
+
+      <el-button style="margin-left: 10px;" type="primary" @click="openJsonDialog()" icon="el-icon-tickets">
+      导出路段信息
+      </el-button>
+      <el-dialog
+      :title="jsonTitle"
+      :visible.sync="jsonVisible"
+      :append-to-body="true"
+      width="50%">
+      <div>
+          确认导出当前路段信息？
+      </div>
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="jsonVisible = false">取消</el-button>
+      <el-button type="primary" @click="json">导出</el-button>
+      </span>
+      </el-dialog>
+    </div>
     <el-table
       :data="tableData"
       stripe
@@ -61,6 +99,7 @@
 
 <script>
 import {config} from '../../../config/config.js'
+import csv2arr from '@/assets/csv-arr'
 export default {
   inject: ['reload'],
   data() {
@@ -71,11 +110,43 @@ export default {
         roadNumFilter: [],
         lengthFilter: [],
         levelFilter: [],
-        typeFilter: []
+        typeFilter: [],
+        csvTitle: '导入CSV文件',
+        jsonTitle: '导出CSV文件',
+        csvVisible: false,
+        jsonVisible: false
       };
     },
   methods: {
-    
+    openCsvDialog() {
+      this.csvVisible = true;
+      this.csvTitle = '导入CSV文件';
+    },
+    openJsonDialog() {
+      this.jsonVisible = true;
+      this.jsonTitle = '导出CSV文件';
+    },
+    csv() {
+      if(this.$refs.csvData.files.length!=1){
+        this.$alert("一次只能导入一个csv文件，您当前已导入" + this.$refs.csvData.files.length + "个文件", '注意⚠️', {
+          confirmButtonText: '确定',})
+      }
+      else if(this.$refs.csvData.files[0].name.split('.').pop()!="csv"){
+        this.$alert("您选择的不是csv文件，请重新选择！", '注意⚠️', {
+          confirmButtonText: '确定',})
+      }
+      else{
+        csv2arr.csv(this.$refs.csvData.files[0]).then((res)=>{
+          console.log('我的数据', res)
+        })
+        var test = document.getElementById('fileupload');
+        test.value = ''
+        this.csvVisible = false;
+      }
+    },
+    json() {
+
+    }
   },
   mounted(){
     var self = this
